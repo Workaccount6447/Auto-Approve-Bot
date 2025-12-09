c7f2b9055b8347b242e34a1bc5e72449dbe1dd11
Workaccount6447 <sndj86779@gmail.com>
2025-12-09 14:37:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ DB_URI = os.environ.get("DB_URI", "mongodb+srv://highlootdeald_db_user:<db_passw
 DB_NAME = os.environ.get("DB_NAME", "request")
 
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1003499488824").split())) # Add Multiple channel ids
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "").split())) # Add Multiple channel ids
