31a262cd082b8f4a703dffa3cc483687c864ba9a
Workaccount6447 <sndj86779@gmail.com>
2025-12-19 21:59:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ PICS = (os.environ.get("PICS", "https://filehosting.kustbotsweb.workers.dev/f/ca
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003386051877"))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://dealsduniyalimited_db_user:RwEd6eiGaWdeRVzx@cluster0.pg8j47e.mongodb.net/?appName=Cluste")
 DB_NAME = os.environ.get("DB_NAME", "RoyalityBots")
 
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
