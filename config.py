63b9726179224a710090db287785e39a874d619d
Workaccount6447 <sndj86779@gmail.com>
2025-12-19 22:01:35 +0530

    Update default value for NEW_REQ_MODE
    Changed default value of NEW_REQ_MODE to 'true'.

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ ADMIN = int(os.environ.get("ADMIN", "8420494874"))
 PICS = (os.environ.get("PICS", "https://filehosting.kustbotsweb.workers.dev/f/ca44a44d52b04241ae77632b8ec4e484")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003386051877"))
-NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
+NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "true").lower() == "true"  # Set "True" For accept new requests
 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://dealsduniyalimited_db_user:RwEd6eiGaWdeRVzx@cluster0.pg8j47e.mongodb.net/?appName=Cluste")
 DB_NAME = os.environ.get("DB_NAME", "RoyalityBots")
